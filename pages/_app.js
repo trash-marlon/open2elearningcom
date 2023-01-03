458878213a3a9fe74ca010b2efa49232a1c026a3
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-03 23:33:44 +0100

    sc

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Maintenance from '../src/components/Maintenance'
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Script from "next/script";
+import Head from "next/head";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log("NEXT_MAINTENANCE_MODE: ",  process.env.NEXT_MAINTENANCE_MODE)
+
+  if (process.env.MAINTENANCE_MODE === 'false') {
+     return (
+     <> 
+        <Component {...pageProps} />
+     </>
+     )
+  }
+
+  else {
+    return <Maintenance/>
+  }
+  
 }
 
 export default MyApp
---
+++ src/components/Maintenance/Maintenance.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Maintenance() {
+  return (
+    <div class="d-flex flex-column h-100 maintenance">
+      <main class="flex-shrink-0">
+        <div class="container">
+          <h1 class="mt-5">open2elearning.com</h1>
+          <h3 class="mt-5">Our Website is Coming Soon</h3>
+          <p class="lead">We are working hard to finish the development of this site.
+           </p>
+        </div>
+      </main>
+
+      <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+          <span class="text-muted">Marlon Falcon Hernandez</span>
+        </div>
+      </footer>
+  </div>
+  )
+}
