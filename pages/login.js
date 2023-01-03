d980537023da3fb5142be24a2b084e114dc3257a
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-04 00:53:28 +0100

    sc

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,27 @@ import BasicLayout from '../src/layouts/BasicLayout'
 export default function Login() {
   return (
     <BasicLayout>
-      <h1>Login</h1>
+      <div className="card mx-auto login-box">
+      <div className="card-body">
+      <h4 className="card-title mb-4">Entrar</h4>
+      <form>
+          <div className="form-group">
+			 <input type="email" className="form-control" placeholder="Email Address"/>
+          </div>
+          <div className="form-group">
+			<input type="password" className="form-control" placeholder="Password"/>
+          </div>
+          
+          <div className="form-group">
+          	<a href="#" className="float-right">Forgot password?</a> 
+           
+          </div>
+          <div className="form-group">
+              <button type="submit" className="btn btn-primary btn-block"> Login  </button>
+          </div>  
+      </form>
+      </div>
+    </div>
     </BasicLayout>
   )
 }
